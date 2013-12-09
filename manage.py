1462f53056cd950f86939e48c49d509d5670603c
Thomas Recouvreux <thomas.recouvreux@gmail.com>
2013-12-09 13:03:51 -0500

    Split app and static, only static is visible by web

--- manage.py
+++ manage.py
@@ -3,25 +3,50 @@
 import sys
 import os
 import sh
+import pwd
 
 from scripts.utils import *
 
 def create_site(site):
+    '''
+    Create the site directory from the template
+    1. copy the template
+    2. change permissions
+    
+    /               rwxr-xr-x   site:site_grp
+    |-- app         rwx------   site:site_grp
+    `-- static      rwxr-s---   site:web
+    
+    '''
     ss = loc_site(site)
-    gid = 'web'
+    gid = pwd.getpwnam(site).pw_gid
     print "Create site dir for %s..." % site,
     if os.path.exists(ss):
         print 'Already existing'
     else:
-        sh.cp('-a', SITE_TEMPLATE_PATH, ss)
+        sh.cp('-a', PATH_SITE_TEMPLATE, ss)
         print "OK"
+    # process root
     print 'chown %s:%s %s ...' % (site, gid, ss),
     sh.chown('-R', '%s:%s' % (site, gid), ss)
     print 'Ok'
-    print 'chmod 2750 %s ...' % ss,
-    sh.chmod('2750', ss)
+    print 'chmod 755 %s ...' % ss,
+    sh.chmod('755', ss)
     print 'Ok'
-    
+    # process app dir
+    app_dir = loc_app_dir(site)
+    print 'chmod 700 %s ...' % app_dir,
+    sh.chmod('700', app_dir)
+    print 'Ok'
+    # process static dir
+    static_dir = loc_static_dir(site)
+    print 'chown %s:%s %s ...' % (site, WEB_GID, static_dir),
+    sh.chown('-R', '%s:%s' % (site, WEB_GID), static_dir)
+    print 'Ok'
+    print 'chmod 2750 %s ...' % static_dir,
+    sh.chmod('2750', static_dir)
+    print 'Ok'
+
 
 def enable_site(site):
     uc = loc_uwsgi_conf(site)
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,28 +1,51 @@
-
 import sys
 import os
 
+# scripts directory
 ROOT_SCRIPTS = os.path.abspath(os.path.dirname(__file__))
+# project root directory
 ROOT = os.path.abspath(os.path.join(ROOT_SCRIPTS, '..'))
+# project config directory
 ROOT_CONF = os.path.join(ROOT, 'conf')
+# uwsgi directory
 ROOT_UWSGI = os.path.join(ROOT, 'uwsgi')
+# template directory
 ROOT_TEMPLATES = os.path.join(ROOT, 'templates')
+# uwsgi conf directory
 ROOT_UWSGI_CONF = os.path.join(ROOT_UWSGI, 'conf')
+# vassals directory
 ROOT_UWSGI_VASSALS = os.path.join(ROOT_UWSGI, 'vassals')
 
+# path to the site template
 PATH_SITE_TEMPLATE = os.path.join(ROOT_TEMPLATES, 'site')
+# path to the vassal template
 PATH_VASSAL_TEMPLATE = os.path.join(ROOT_TEMPLATES, 'vassal.ini')
+# path to the config for sites root 
 PATH_SITES_PATH = os.path.join(ROOT_CONF, 'sites_path.txt')
+# path to the default vassal config
 PATH_VASSAL_CONF = os.path.join(ROOT_UWSGI_CONF, 'vassal.ini')
 
+# web group id
+WEB_GID = 'web'
 
 def loc_site(site):
+    ''' get the path to the root directory of a site '''
     sites_path = open(PATH_SITES_PATH).read()
     return os.path.join(sites_path, site)
 
 def loc_uwsgi_conf(site):
+    ''' get the path to the uwsgi config of a site '''
     return os.path.join(ROOT_UWSGI_VASSALS, site+'.ini')
 
+def loc_app_dir(site):
+    return os.path.join(loc_site(site), 'app')
+
+def loc_static_dir(site):
+    return os.path.join(loc_site(site), 'static')
+
+def loc_wsgi_dir(site):
+    return os.path.join(loc_app_dir(site), 'wsgi')
+    
 
 def _ask_root_sites():
     def ask_sites_path():
@@ -33,6 +56,7 @@ def _ask_root_sites():
         sites_path = ask_sites_path()
     return sites_path
 
+# compute the rott directory of sites
 if os.path.exists(PATH_SITES_PATH):
     ROOT_SITES = open(PATH_SITES_PATH).read()
 else:
